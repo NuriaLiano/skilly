f985635ed2e2edd106d3a69d545ab84e730e6a68
Nuria Liaño <lianonuria@gmail.com>
2024-01-21 20:13:08 +0100

    remove files

--- RandomCircles.java
+++
@@ -1,7 +0,0 @@
-public class RandomCircles {
-    public static void main(String[] args) {
-    CircleModel model = new CircleModel();
-    CircleView vista = new CircleView(model);
-    new CircleController(model, vista);
-    vista.setVisible(true);
-    }}
